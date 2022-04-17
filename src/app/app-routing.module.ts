bcab5ce49d39efc04faf110108dd9e637a2d3a0b
Karenkoech <karenkoech3@gmail.com>
2022-04-17 09:38:04 -0400

    add the userform route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserformComponent } from './userform/userform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:'userform',component:UserformComponent},
+  {path:'',redirectTo:'/userform',pathMatch:'full'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [UserformComponent,];

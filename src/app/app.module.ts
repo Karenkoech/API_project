00c00561fe1b256844bd44223b384b4ebc20e6c2
Karenkoech <karenkoech3@gmail.com>
2022-04-18 10:56:46 -0400

    create new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule,  } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitAppComponent } from './git-app/git-app.component';
+import { GitRepoComponent } from './git-repo/git-repo.component';
+import { GitRepocardComponent } from './git-repocard/git-repocard.component';
+import { RepodataComponent } from './repodata/repodata.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitAppComponent,
+    GitRepoComponent,
+    GitRepocardComponent,
+    RepodataComponent,
+    RepositoryComponent,
+    DashboardComponent,
+    ProfileComponent,
+    RepositoriesComponent
     
   ],
   imports: [

1e8c9f55240d2d4580ea8a4ec02446e544857239
Karenkoech <karenkoech3@gmail.com>
2022-04-18 16:25:57 -0400

    give functionality to user search functions

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RepoSearchService } from '../repo-search.service';
+
+
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: any;
+  userRepos:any;
+  username: string = 'karenkoech'
+  imageWidth: number = 150;
+  imageHeight: number = 200;
+  
 
-  constructor() { }
+  constructor( private repoSearchService: RepoSearchService) { }
 
-  ngOnInit(): void {
+  findUser () {
+    this.repoSearchService.UpdateUser(this.username);
+
+    this.repoSearchService.getUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+
+    this.repoSearchService.getUserRepos().subscribe(repos => {
+      console.log(repos);
+      this.userRepos = repos;
+    })
+  }
+  ngOnInit() {
+    this.findUser();
   }
 
+
+  
+
+  
 }
